5ef0169533ad6a61d630e9f8fcfa36226c145d6d
Joel Varty <joelvarty@outlook.com>
2020-04-27 11:20:38 -0400

    initial redirects

--- functions/redirect/redirect.js
+++ functions/redirect/redirect.js
@@ -1,12 +1,27 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+	const previewQuery = event.queryStringParameters.agilitypreview
+	const channelID = event.queryStringParameters.AgilityChannelID
+	const lang = event.queryStringParameters.lang
+
+	const path = event.path.substring ("/.netlify/functions/redirect".length)
+
+	const baseURL = "https://agilitycms-uat.azurewebsites.net"
+	const gatsbyURL = "https://agility-website-gatsby-1445577254.gtsb.io"
+
+	let redirectUrl = "";
+
+	//if not Gatsby
+	redirectUrl = `${baseURL}${path}?agilitypreviewkey=${ encodeURIComponent(previewQuery) }`
+
+
+	//https://agilitycms-preview.netlify.app/.netlify/functions/redirect/product/why-agility?AgilityChannelID=1&lang=en-ca&agilitypreviewkey=%2b8njrinSYiOFEXvr1SdYXZ8B4P8WNLaRwQojpCY%2beeJP4U2OZlBUWyQADkWl22ipb%2bavkFT%2fkQE95I0cDRB%2f5Q%3d%3d&agilityts=20200427111501
     return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
+      statusCode: 302,
+      body: `Redirect to ${redirectUrl}`,
       // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
+    headers: { "location": redirectUrl},
       // isBase64Encoded: true,
     }
   } catch (err) {
